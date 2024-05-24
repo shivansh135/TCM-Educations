fa19aaf9249c3d2a57c5ef68378fea005336b51b
Rohit-Singh-07 <rohitsingh234@lpu.in>
2024-05-24 18:18:30 +0530

    chore: fix link for images in testimonial.js

--- src/pages/Testimonials/stud_testimonial.js
+++ src/pages/Testimonials/stud_testimonial.js
@@ -46,7 +46,7 @@ export function Testimonial() {
     return (
         <div>
             <div className="container">
-                <img src={`./images/${students_data.img}`} alt="stud_img" className="stud_img"/>
+                <img src={`/images/${students_data.img}`} alt="stud_img" className="stud_img"/>
                 <div className="container_text">
                     <button className="btn">{students_data.ile}</button>
                     <div className="stud_name">
@@ -64,16 +64,16 @@ export function Testimonial() {
             </div>
 
             <div className="road_map">
-            <img src="./images/Roadmap detail.png" alt="road_map" className="road_map_img"/>
-            <img src="./images/IIM.png" alt="road_map" className="IIM_img"/>
+            <img src="/images/Roadmap detail.png" alt="road_map" className="road_map_img"/>
+            <img src="/images/IIM.png" alt="road_map" className="IIM_img"/>
             </div>
 
             <HeadingSubheading heading={` From Aspirants to Achievers`} sub={`We're India's leading coaching institute, empowering students to conquer CAT, IPMAT, CLAT & CUET with our expert faculty, personalized approach, and proven strategies. We'll equip you with the knowledge and confidence to ace your exams and gain admission to the top colleges in India.`}/>
 
 
             <div className='card-2-container'>
-            <Card_two src='./images/student1.png' name='Ajai Singh' cor='CAT' ile='97.81%ile' review="TCM Special Clone Test Series and suggestions of Ajai Sir acted as a boon in my preparation. Looking back, I can't help but appreciate the support that TCM provided to me." />
-            <Card_two src='./images/student1.png' name='Ajai Singh' cor='CAT' ile='97.81%ile' review="TCM Special Clone Test Series and suggestions of Ajai Sir acted as a boon in my preparation. Looking back, I can't help but appreciate the support that TCM provided to me." />
+            <Card_two src='/images/student1.png' name='Ajai Singh' cor='CAT' ile='97.81%ile' review="TCM Special Clone Test Series and suggestions of Ajai Sir acted as a boon in my preparation. Looking back, I can't help but appreciate the support that TCM provided to me." />
+            <Card_two src='/images/student1.png' name='Ajai Singh' cor='CAT' ile='97.81%ile' review="TCM Special Clone Test Series and suggestions of Ajai Sir acted as a boon in my preparation. Looking back, I can't help but appreciate the support that TCM provided to me." />
             </div>
 
             <div className='FQA'>
